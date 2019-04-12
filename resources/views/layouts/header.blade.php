c58a44cc12cdef73610792ba8e000ce8ddab2509
putudyatmika <pdyatmika@gmail.com>
2019-04-13 01:01:17 +0800

    v0.4.0
    update header dan logo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-default navbar-static-top m-b-0">
             <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="ti-menu"></i></a>
                 <div class="top-left-part hidden-xs">
-                <!---<a class="logo" href="{{url('')}}"><b><img src="{{ asset('tema/plugins/images/eliteadmin-logo.png')}}" alt="home" /></b><span class="hidden-xs"><img src="{{ asset('tema/plugins/images/eliteadmin-text.png')}}" alt="home" /></span></a>--->
-                <a class="logo" href="{{url('')}}">&nbsp;</a>
+                <a class="logo" href="{{url('')}}"><b><img src="{{ asset('img/perjadin-logo-putih.png')}}" alt="home" /></b><span class="hidden-xs"><img src="{{ asset('img/perjadin-text-kecil.png')}}" alt="home" /></span></a>
+
                 </div>
                 <ul class="nav navbar-top-links navbar-left hidden-xs">
                     <li><a href="javascript:void(0)" class="open-close hidden-xs waves-effect waves-light"><i class="icon-arrow-left-circle ti-menu"></i></a></li>
